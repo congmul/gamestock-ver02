3a43b9c283a0efd561cff0456559fb648d900fef
Jehyun <congmul@hotmail.com>
2021-09-04 13:50:32 -0700

    Add comment for GetFakeDate()

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -30,7 +30,8 @@ export function capitalizeFirstLetter(string) {
 
 export function GetFakeDate() {
   // This is for Presentation (Manupulating DATE)
-  //It is always This Year / This Month / Today / 13:07:00
+  //It is always This Year / This Month / Today (date) / 13:07:00
+  // ex) 2021 / 9 / 4 / 13:07:00
   let thisYear = (new Date().getFullYear()).toString();
   let thisMonth = (new Date().getMonth()).toString();
   let today = (new Date().getDate()).toString();
