d684f79f14c3e61a6e37039132f9c22b70be3c12
Jehyun <congmul@hotmail.com>
2021-09-09 07:36:13 -0700

    Merge Chart of Stock and Watch list

--- client/src/components/MyStockList/StockChart/index.js
+++ client/src/components/MyStockList/StockChart/index.js
@@ -1,60 +1,60 @@
-import React from 'react'
-import Plot from 'react-plotly.js';
+// import React from 'react'
+// import Plot from 'react-plotly.js';
 
 
-export default function ChartUser(props) {
-    // console.log("In ChartUserInfo")
-    // console.log(props.company.graphData)
+// export default function ChartUser(props) {
+//     // console.log("In ChartUserInfo")
+//     // console.log(props.company.graphData)
 
-    return (
-        <Plot
-            data={[
-                {
-                    x: props.company.graphData.x,
-                    y: props.company.graphData.y,
-                    type: 'scatter',
-                    mode: 'lines',
-                    marker: props.company.graphData.marker,
-                },
-            ]}
+//     return (
+//         <Plot
+//             data={[
+//                 {
+//                     x: props.company.graphData.x,
+//                     y: props.company.graphData.y,
+//                     type: 'scatter',
+//                     mode: 'lines',
+//                     marker: props.company.graphData.marker,
+//                 },
+//             ]}
 
-            layout={{
-                dragmode: 'zoom',
-                margin: {
-                    l: 0,
-                    r: 0,
-                    b: 0,
-                    t: 0
-                },
-                xaxis: {
-                    // TODO:get range date!!,
-                    range: [props.company.graphData.x[0], props.company.graphData.x[props.company.graphData.x.length -1]],
-                    rangeslider: {
-                        visible: false
-                    },
-                    type: 'date',
-                    // type: props.type,
-                    visible: false,
-                    tickmode: 'array',
-                    // fixedrange: true,
-                    // autorange: "reversed"
+//             layout={{
+//                 dragmode: 'zoom',
+//                 margin: {
+//                     l: 0,
+//                     r: 0,
+//                     b: 0,
+//                     t: 0
+//                 },
+//                 xaxis: {
+//                     // TODO:get range date!!,
+//                     range: [props.company.graphData.x[0], props.company.graphData.x[props.company.graphData.x.length -1]],
+//                     rangeslider: {
+//                         visible: false
+//                     },
+//                     type: 'date',
+//                     // type: props.type,
+//                     visible: false,
+//                     tickmode: 'array',
+//                     // fixedrange: true,
+//                     // autorange: "reversed"
 
-                },
-                yaxis: {
-                    visible: false
-                },
-                font: {
-                    family: "Raleway",
-                    color: "#fff"
-                },
-                // width: 100,
-                // height: 30,
-                paper_bgcolor: "#000",
-                plot_bgcolor: "#000",
-            }}
-            style={{width:"100%", height:30}}
-            config={{ displayModeBar: false }}
-            useResizeHandler={ true }
-        />
-    )
-}
+//                 },
+//                 yaxis: {
+//                     visible: false
+//                 },
+//                 font: {
+//                     family: "Raleway",
+//                     color: "#fff"
+//                 },
+//                 // width: 100,
+//                 // height: 30,
+//                 paper_bgcolor: "#000",
+//                 plot_bgcolor: "#000",
+//             }}
+//             style={{width:"100%", height:30}}
+//             config={{ displayModeBar: false }}
+//             useResizeHandler={ true }
+//         />
+//     )
+// }
